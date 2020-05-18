7180b07d276237b1e2fc759fce189ecd943c3453
Josef Duchesne <josefduchesne@outlook.com>
2020-05-18 01:00:07 -0400

    Version bumo for install fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name="subset",
-      version="0.1.1",
+      version="0.1.2",
       description="A cli-based word game",
       long_description_content_type="text/markdown",
       long_description=long_description,
